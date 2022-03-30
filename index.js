39dd9b5b343724f5c87eeb3bda92ea92758087f6
Gaurav Kumar <gauravkr2179@gmail.com>
2022-03-30 12:33:26 +0530

    mycommit

---
+++ index.js
@@ -0,0 +1,46 @@
+// var chemit = JSON.parse(localStorage.getItem("chemist")) || [];
+// console.log(chemit)
+
+// let dair = JSON.parse(localStorage.getItem("dairy")) || [];
+// console.log(dair)
+
+
+const display = (data, main) => {
+    main.innerHTML = null;
+
+    data.forEach((el) => {
+        let div = document.createElement("div");
+        div.setAttribute("id", "cont")
+        let imgDiv = document.createElement("div");
+
+        let img = document.createElement("img");
+        img.src = el.img_url;
+        imgDiv.append(img)
+        let nameDiv = document.createElement("div");
+        nameDiv.setAttribute("class", "info")
+
+        let name = document.createElement("p");
+        name.innerText = el.product_name
+
+        let qty = document.createElement("p");
+        qty.innerText = el.qty
+
+        nameDiv.append(name, qty);
+
+        let priceDiv = document.createElement("div");
+        priceDiv.setAttribute("class", "price")
+
+        let price = document.createElement("span")
+        price.innerText = `₹${el.price}`;
+
+        let strikePrice = document.createElement("span");
+        strikePrice.innerText = `₹${el.strikeprice}`;
+
+        priceDiv.append(price, strikePrice);
+        div.append(imgDiv, nameDiv, priceDiv)
+        main.append(div);
+    })
+}
+
+export default display;
+// display(dair)
