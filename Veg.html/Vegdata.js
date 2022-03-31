f244fd33479a7d8254e6dadb3ba66c1c20094a90
Ayush Anshu <ayushanshu56@gmail.com>
2022-04-01 03:07:00 +0530

    Create Vegdata.js

---
+++ Veg.html/Vegdata.js
@@ -0,0 +1,459 @@
+//  let Veg= [
+//     {
+//      "name": "",
+//      "url": "https://blinkit.com/prn/onion-1-kg/prid/391306",
+//      "discount": "Onion 1 kg",
+//      "discount_url": "https://blinkit.com/prn/onion-1-kg/prid/391306",
+//      "weight": "1 kg",
+//      "weight_url": "https://blinkit.com/prn/onion-1-kg/prid/391306",
+//      "price": "₹34",
+//      "price_url": "https://blinkit.com/prn/onion-1-kg/prid/391306",
+//      "strikedP": "₹ 42",
+//      "strikedP_url": "https://blinkit.com/prn/onion-1-kg/prid/391306"
+//     },
+//     {
+//      "name": "",
+//      "url": "https://blinkit.com/prn/green-chilli/prid/423735",
+//      "discount": "Green Chilli",
+//      "discount_url": "https://blinkit.com/prn/green-chilli/prid/423735",
+//      "weight": "100 g",
+//      "weight_url": "https://blinkit.com/prn/green-chilli/prid/423735",
+//      "price": "₹11",
+//      "price_url": "https://blinkit.com/prn/green-chilli/prid/423735"
+//     },
+//     {
+//         "name": "",
+//         "url": "https://blinkit.com/prn/hybrid-tomato/prid/3881",
+//         "discount": "Hybrid Tomato",
+//         "discount_url": "https://blinkit.com/prn/hybrid-tomato/prid/3881",
+//         "weight": "500 g",
+//         "weight_url": "https://blinkit.com/prn/hybrid-tomato/prid/3881",
+//         "price": "₹15",
+//         "price_url": "https://blinkit.com/prn/hybrid-tomato/prid/3881",
+//         "strikedP": "₹ 23",
+//         "strikedP_url": "https://blinkit.com/prn/hybrid-tomato/prid/3881"
+//        },
+//        {
+//         "name": "",
+//         "url": "https://blinkit.com/prn/desi-tomato/prid/445675",
+//         "discount": "Desi Tomato",
+//         "discount_url": "https://blinkit.com/prn/desi-tomato/prid/445675",
+//         "weight": "500 g",
+//         "weight_url": "https://blinkit.com/prn/desi-tomato/prid/445675",
+//         "price": "₹18",
+//         "price_url": "https://blinkit.com/prn/desi-tomato/prid/445675",
+//         "strikedP": "₹ 19",
+//         "strikedP_url": "https://blinkit.com/prn/desi-tomato/prid/445675"
+//        },
+//        {
+//         "name": "",
+//         "url": "https://blinkit.com/prn/baby-bottle-gourd-lauki-small/prid/448913",
+//         "discount": "Baby Bottle Gourd (Lauki) - Smal…",
+//         "discount_url": "https://blinkit.com/prn/baby-bottle-gourd-lauki-small/prid/448913",
+//         "weight": "1 piece (250 g - 350 g)",
+//         "weight_url": "https://blinkit.com/prn/baby-bottle-gourd-lauki-small/prid/448913",
+//         "price": "₹20",
+//         "price_url": "https://blinkit.com/prn/baby-bottle-gourd-lauki-small/prid/448913",
+//         "strikedP": "₹ 22",
+//         "strikedP_url": "https://blinkit.com/prn/baby-bottle-gourd-lauki-small/prid/448913"
+//        },
+//        {
+//         "name": "",
+//         "url": "https://blinkit.com/prn/lady-finger/prid/3898",
+//         "discount": "Lady Finger",
+//         "discount_url": "https://blinkit.com/prn/lady-finger/prid/3898",
+//         "weight": "250 g",
+//         "weight_url": "https://blinkit.com/prn/lady-finger/prid/3898",
+//         "price": "₹22",
+//         "price_url": "https://blinkit.com/prn/lady-finger/prid/3898",
+//         "strikedP": "₹ 39",
+//         "strikedP_url": "https://blinkit.com/prn/lady-finger/prid/3898"
+//        },
+//        {
+//         "name": "",
+//         "url": "https://blinkit.com/prn/cauliflower-gobhi/prid/445396",
+//         "discount": "Cauliflower (Gobhi)",
+//         "discount_url": "https://blinkit.com/prn/cauliflower-gobhi/prid/445396",
+//         "weight": "500 g - 700 g",
+//         "weight_url": "https://blinkit.com/prn/cauliflower-gobhi/prid/445396",
+//         "price": "₹29",
+//         "price_url": "https://blinkit.com/prn/cauliflower-gobhi/prid/445396",
+//         "strikedP": "₹ 35",
+//         "strikedP_url": "https://blinkit.com/prn/cauliflower-gobhi/prid/445396"
+//        },
+//        {
+//         "name": "",
+//         "url": "https://blinkit.com/prn/chilli-organically-grown/prid/467648",
+//         "discount": "Chilli - Organically grown",
+//         "discount_url": "https://blinkit.com/prn/chilli-organically-grown/prid/467648",
+//         "weight": "100 g",
+//         "weight_url": "https://blinkit.com/prn/chilli-organically-grown/prid/467648",
+//         "price": "₹30",
+//         "price_url": "https://blinkit.com/prn/chilli-organically-grown/prid/467648",
+//         "strikedP": "₹ 33",
+//         "strikedP_url": "https://blinkit.com/prn/chilli-organically-grown/prid/467648"
+//        },
+//        {
+//         "name": "",
+//         "url": "https://blinkit.com/prn/kale-leaves/prid/447624",
+//         "discount": "Kale Leaves",
+//         "discount_url": "https://blinkit.com/prn/kale-leaves/prid/447624",
+//         "weight": "100 g - 125 g",
+//         "weight_url": "https://blinkit.com/prn/kale-leaves/prid/447624",
+//         "price": "₹57",
+//         "price_url": "https://blinkit.com/prn/kale-leaves/prid/447624",
+//         "strikedP": "₹ 64",
+//         "strikedP_url": "https://blinkit.com/prn/kale-leaves/prid/447624"
+//        },
+//        {
+//         "name": "",
+//         "url": "https://blinkit.com/prn/ginger-adrak/prid/421703",
+//         "discount": "Ginger (Adrak)",
+//         "discount_url": "https://blinkit.com/prn/ginger-adrak/prid/421703",
+//         "weight": "250 g",
+//         "weight_url": "https://blinkit.com/prn/ginger-adrak/prid/421703",
+//         "price": "₹19",
+//         "price_url": "https://blinkit.com/prn/ginger-adrak/prid/421703",
+//         "strikedP": "₹ 23",
+//         "strikedP_url": "https://blinkit.com/prn/ginger-adrak/prid/421703"
+//        },
+//        {
+//         "name": "",
+//         "url": "https://blinkit.com/prn/ivy-gourd-kundru/prid/3907",
+//         "discount": "Ivy Gourd (Kundru)",
+//         "discount_url": "https://blinkit.com/prn/ivy-gourd-kundru/prid/3907",
+//         "weight": "250 g",
+//         "weight_url": "https://blinkit.com/prn/ivy-gourd-kundru/prid/3907",
+//         "price": "₹17",
+//         "price_url": "https://blinkit.com/prn/ivy-gourd-kundru/prid/3907",
+//         "strikedP": "₹ 28",
+//         "strikedP_url": "https://blinkit.com/prn/ivy-gourd-kundru/prid/3907"
+//        },
+//        {
+//         "name": "",
+//         "url": "https://blinkit.com/prn/local-carrot-gajar/prid/332740",
+//         "discount": "Local Carrot (Gajar)",
+//         "discount_url": "https://blinkit.com/prn/local-carrot-gajar/prid/332740",
+//         "weight": "500 g",
+//         "weight_url": "https://blinkit.com/prn/local-carrot-gajar/prid/332740",
+//         "price": "₹26",
+//         "price_url": "https://blinkit.com/prn/local-carrot-gajar/prid/332740",
+//         "strikedP": "₹ 32",
+//         "strikedP_url": "https://blinkit.com/prn/local-carrot-gajar/prid/332740"
+//        },
+//        {
+//         "name": "",
+//         "url": "https://blinkit.com/prn/green-peas-organically-grown/prid/467660",
+//         "discount": "Green Peas - Organically grow…",
+//         "discount_url": "https://blinkit.com/prn/green-peas-organically-grown/prid/467660",
+//         "weight": "250 g",
+//         "weight_url": "https://blinkit.com/prn/green-peas-organically-grown/prid/467660",
+//         "price": "₹51",
+//         "price_url": "https://blinkit.com/prn/green-peas-organically-grown/prid/467660",
+//         "strikedP": "₹ 61",
+//         "strikedP_url": "https://blinkit.com/prn/green-peas-organically-grown/prid/467660"
+//        },
+//        {
+//         "name": "",
+//         "url": "https://blinkit.com/prn/french-beans/prid/4916",
+//         "discount": "French Beans",
+//         "discount_url": "https://blinkit.com/prn/french-beans/prid/4916",
+//         "weight": "250 g",
+//         "weight_url": "https://blinkit.com/prn/french-beans/prid/4916",
+//         "price": "₹13",
+//         "price_url": "https://blinkit.com/prn/french-beans/prid/4916",
+//         "strikedP": "₹ 20",
+//         "strikedP_url": "https://blinkit.com/prn/french-beans/prid/4916"
+//        },
+//        {
+//         "name": "",
+//         "url": "https://blinkit.com/prn/ginger-adrak-organically-grown/prid/467652",
+//         "discount": "Ginger (Adrak) - Organically grow…",
+//         "discount_url": "https://blinkit.com/prn/ginger-adrak-organically-grown/prid/467652",
+//         "weight": "100 g",
+//         "weight_url": "https://blinkit.com/prn/ginger-adrak-organically-grown/prid/467652",
+//         "price": "₹9",
+//         "price_url": "https://blinkit.com/prn/ginger-adrak-organically-grown/prid/467652",
+//         "strikedP": "₹ 10",
+//         "strikedP_url": "https://blinkit.com/prn/ginger-adrak-organically-grown/prid/467652"
+//        },
+//        {
+//         "name": "",
+//         "url": "https://blinkit.com/prn/bottle-gourd-lauki/prid/217614",
+//         "discount": "Bottle Gourd (Lauki)",
+//         "discount_url": "https://blinkit.com/prn/bottle-gourd-lauki/prid/217614",
+//         "weight": "1 piece (400 g - 600 g)",
+//         "weight_url": "https://blinkit.com/prn/bottle-gourd-lauki/prid/217614",
+//         "price": "₹26",
+//         "price_url": "https://blinkit.com/prn/bottle-gourd-lauki/prid/217614",
+//         "strikedP": "₹ 40",
+//         "strikedP_url": "https://blinkit.com/prn/bottle-gourd-lauki/prid/217614"
+//        },
+//        {
+//         "name": "",
+//         "url": "https://blinkit.com/prn/garlic-lehsun/prid/95031",
+//         "discount": "Garlic (Lehsun)",
+//         "discount_url": "https://blinkit.com/prn/garlic-lehsun/prid/95031",
+//         "weight": "200 g",
+//         "weight_url": "https://blinkit.com/prn/garlic-lehsun/prid/95031",
+//         "price": "₹14",
+//         "price_url": "https://blinkit.com/prn/garlic-lehsun/prid/95031",
+//         "strikedP": "₹ 17",
+//         "strikedP_url": "https://blinkit.com/prn/garlic-lehsun/prid/95031"
+//        },
+//        {
+//         "name": "",
+//         "url": "https://blinkit.com/prn/mint-leaves-pudina/prid/3914",
+//         "discount": "Mint Leaves (Pudina)",
+//         "discount_url": "https://blinkit.com/prn/mint-leaves-pudina/prid/3914",
+//         "weight": "200 g",
+//         "weight_url": "https://blinkit.com/prn/mint-leaves-pudina/prid/3914",
+//         "price": "₹14",
+//         "price_url": "https://blinkit.com/prn/mint-leaves-pudina/prid/3914",
+//         "strikedP": "₹ 18",
+//         "strikedP_url": "https://blinkit.com/prn/mint-leaves-pudina/prid/3914"
+//        }
+// ]
+let Veg=[{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/217614a.jpg",
+    weight:"1 Piece",
+    name:"Bottle Gourd (lauki)",
+    price:"₹58"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/467673a.jpg",
+    name:"Garlic-Peeled",
+    price:"₹43",
+    weight:"200g"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/469737a.jpg",
+    name:"Red Potato",
+    price:"₹32",
+    weight:"1Kg"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/39395a.jpg",
+    name:"Yellow Capsicum",
+    price:"₹49",
+    weight:"1 Piece"
+
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/199435a.jpg",
+    name:"Potato",
+    price:"₹18", 
+    weight:"1Kg"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/428608a.jpg",
+    name:"Amla",
+    price:"₹52",
+    weight:"250gm"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/145447a.jpg",
+    name:"Sambhar Onion",
+    price:"₹11",
+    weight:"250gm"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/23379a.jpg",
+    name:"Sweet Corn-Packet",
+    price:"₹44",
+    weight:"180gm"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/118917a.jpg",
+    name:"Chandramukhi Potato",
+    price:"₹34",
+    weight:"1Kg"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/4926a.jpg",
+    name:"Baby Potato",
+    price:"₹10",
+    weight:"500gm"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/3885a.jpg",
+    name:"Colocasia",
+    price:"₹17",
+    weight:"250gm"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/326343a.jpg",
+    name:"Yam",
+    price:"₹29",
+    weight:"250gm"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/16405a.jpg",
+    name:"Raw Banana",
+    price:"₹52",
+    weight:"6 Piece"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/137816a.jpg",
+    name:"Baby Corn-Packet",
+    price:"₹33",
+    weight:""
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/4927a.jpg",
+    name:"Bitter Gourd(Karela)",
+    price:"₹17",
+    weight:"200gm"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/363887a.jpg",
+    name:"American Sweet Corn",
+    price:"₹51",
+    weight:"2 Piece"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/339152a.jpg",
+    name:"Cabbage",
+    price:"₹19",
+    weight:"1 Piece"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/189833a.jpg",
+    name:"Red Cabbage",
+    price:"₹42",
+    weight:"1 Piece"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/3952a.jpg",
+    name:"Sweet Potato",
+    price:"₹35",
+    weight:"500gm"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/203822a.jpg",
+    name:"Raw Turmeric",
+    price:"₹11",
+    weight:"250gm"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/440230a.jpg",
+    name:"Basil Leaves",
+    price:"₹16",
+    weight:"50gm"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/23343a.jpg",
+    name:"English Cucumber",
+    price:"₹71",
+    weight:"500gm"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/39394a.jpg",
+    name:"Red Capsicum",
+    price:"₹49",
+    weight:"1 Piece"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/15220a.jpg",
+    name:"Raw Mango",
+    price:"₹45",
+    weight:"250gm"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/271992a.jpg",
+    name:"Broccoli",
+    price:"₹50",
+    weight:"1 Piece"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/440229a.jpg",
+    name:"Cherry Tomato",
+    price:"₹58",
+    weight:"200gm"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/3881a.jpg",
+    name:"Hybrid Tomato",
+    price:"₹14",
+    weight:"500gm"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/446707a.jpg",
+    name:"Mint Leaves",
+    price:"₹10",
+    weight:"500gm"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/444358a.jpg",
+    name:"Celery",
+    price:"44",
+    weight:"100gm"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/10088a.jpg",
+    name:"Cucumber",
+    price:"₹33",
+    weight:"250gm"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/391306a.jpg",
+    name:"Onion 1Kg",
+    price:"₹35",
+    weight:"500gm"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/3888a.jpg",
+    name:"Green Capsicum",
+    price:"₹42",
+    weight:"1Kg"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/327916a.jpg",
+    name:"Orange Carrot",
+    price:"₹19",
+    weight:"250gm"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/336305a.jpg",
+    name:"Drumstick",
+    price:"₹44",
+    weight:"500gm"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/17643a.jpg",
+    name:"Curry Leaves",
+    price:"₹23",
+    weight:"250gm"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/17643a.jpg",
+    name:"Green Chhilli",
+    price:"₹16",
+    weight:"100gm"
+    
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/438489a.jpg",
+    name:"Picardo Chilli",
+    price:"₹30",
+    weight:"100gm"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/421744a.jpg",
+    name:"Spring Onion",
+    price:"₹25",
+    weight:"250gm"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/439699a.jpg",
+    name:"Lemon",
+    price:"₹55",
+    weight:"150gm"
+},
+{
+    image:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=250,h=250/app/images/products/sliding_image/421742a.jpg",
+    name:"Sanke Gourd",
+    price:"₹25",
+    weight:"250gm"
+}]
+localStorage.setItem("vegdata",JSON.stringify(Veg))
